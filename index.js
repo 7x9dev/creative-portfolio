56597bbb04b722be7a0976311080106b7affdbf3
7x9dev <103652226+7x9dev@users.noreply.github.com>
2022-12-18 20:03:33 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+const audio = document.querySelector("#audio");
+audio.volume = 0.1;
+
+gsap.from(".dscr", {
+   duration: 2,
+   ease: "power1.in",
+   delay: 1.5,
+   opacity: 0,
+});
+
+gsap.from(".links", { delay: 3.5, duration: 1.5, y: -100, opacity: 0 });
+gsap.from(".socials", { delay: 4.5, duration: 1.5, y: 100, opacity: 0 });
+gsap.from(".link, .soc-link", {
+   delay: 5,
+   duration: 2,
+   opacity: 0,
+   stagger: 0.6,
+});
+
+const back = document.querySelector("#btnBack");
+const next = document.querySelector("#btnNext");
+
+const gallery = [
+   "images/1.jpg",
+   "images/2.jpg",
+   "images/3.jpg",
+   "images/4.jpg",
+];
+
+let i = 0;
+
+next.addEventListener("click", () => {
+   i++;
+   if (i > gallery.length - 1) {
+      i = 0;
+   }
+   document.querySelector("#galleryImg").src = gallery[i];
+});
+
+back.addEventListener("click", () => {
+   i--;
+   if (i < 0) {
+      i = gallery.length - 1;
+   }
+   document.querySelector("#galleryImg").src = gallery[i];
+});
